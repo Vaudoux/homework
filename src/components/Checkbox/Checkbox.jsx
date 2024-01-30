4e2730b8b51e67943a08dfad14e882e32a4250a3
Ilya <ildorf@list.ru>
2024-01-30 07:14:45 +0700

    Реализованна функция фильтрации по чекбоксу

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,12 +1,15 @@
 import "./Checkbox.css";
 
-export default function Checkbox(showAdult, sortByAdult) {
+export default function Checkbox({ showAdult, sortByAdult }) {
+  console.log(showAdult);
   return (
     <div className="checkbox">
       <span>Учитывать только совершеннолетних </span>
       <input
         type="checkbox"
-        checked={showAdult} onChange={sortByAdult} className="checkbox"
+        checked={showAdult}
+        onChange={sortByAdult}
+        className="checkbox"
       />
     </div>
   );
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -4,14 +4,41 @@ import { useEffect, useState } from "react";
 import Button from "./Button/Button";
 import Checkbox from "./Checkbox/Checkbox";
 
+const fetching = async () => {
+  return await fetch(
+    "https://mocki.io/v1/a0bd24a9-7d83-4b7f-997d-6193d6a3a726"
+  ).then((response) => response.json());
+};
+
 export default function UserList() {
   const [users, setUsers] = useState([]);
-  /* const [showAdult, setShowAdult] = useState(false); */
+  const [showAdult, setShowAdult] = useState(false);
+
+  const sortByAdult = (data) => {
+    const sortedUsers = data.filter(
+      (user) =>
+        new Date().getFullYear() - new Date(user.birthday).getFullYear() >= 18
+    );
+    setUsers(sortedUsers);
+  };
+
+  const onChange = () => {
+    setShowAdult(!showAdult);
+  };
+
   useEffect(() => {
-    fetch("https://mocki.io/v1/a0bd24a9-7d83-4b7f-997d-6193d6a3a726")
-      .then((response) => response.json())
-      .then((data) => setUsers(data));
+    fetching().then((data) => setUsers(data));
   }, []);
+
+  useEffect(() => {
+    fetching().then((data) => {
+      if (showAdult) {
+        sortByAdult(data);
+      } else {
+        setUsers(data);
+      }
+    });
+  }, [showAdult]);
 
   const sortById = () => {
     const sortedUsers = [...users].sort((a, b) => a.id - b.id);
@@ -30,28 +57,18 @@ export default function UserList() {
     }
     setUsers(sortedUsers);
   };
-  /* const sortByDate = () => {
-    const sortedUsers = [...users].sort(
-      (a, b) => new Date(a.birthday) - new Date(b.birthday)
-    );
-    setUsers(sortedUsers);
-  }; */
   const sortByDate = () => {
     const sortedUsers = [...users].sort(
       (a, b) => new Date(a.birthday) - new Date(b.birthday)
     );
-  
-    // Проверяем, отсортированы ли пользователи в обратном порядке
-    const isSortedDescending = JSON.stringify(users) === JSON.stringify(sortedUsers);
-  
-    // Если пользователи уже отсортированы в обратном порядке, то сортируем их в прямом порядке
+    const isSortedDescending =
+      JSON.stringify(users) === JSON.stringify(sortedUsers);
     if (isSortedDescending) {
       sortedUsers.reverse();
     }
-  
     setUsers(sortedUsers);
   };
-  
+
   return (
     <div>
       <Button
@@ -59,7 +76,7 @@ export default function UserList() {
         sortByName={sortByName}
         sortByDate={sortByDate}
       />
-      <Checkbox /* sortByAdult={sortByAdult} */ />
+      <Checkbox sortByAdult={onChange} showAdult={showAdult} />
       <Table users={users} />
     </div>
   );
